1fa418e17165cc8a1c92d0735d2fb763371187da
Caleb Wood <Calebwjwood60@gmail.com>
2023-08-29 03:33:11 -0500

    completed the routes and solved first issue

--- routes/notes.js
+++ routes/notes.js
@@ -2,3 +2,38 @@ const fb = require('express').Router();
 
 const uuid = require('../helpers/uuid')
 
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+fb.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)))
+})
+
+fb.post('/', (req, res) => {
+    console.info(`${req.method} request received to submit notes`);
+
+    const {noteTitle, noteText} = req.body;
+
+    if (noteTitle && noteText){
+        const newNotes = {
+            noteTitle,
+            noteText,
+            note_id: uuid(),
+        }
+
+        readAndAppend(newNotes, './db/db.json')
+
+        const response = {
+            status: 'success',
+            body: newNotes,
+        }
+
+        res.json(response)
+    }else{
+        res.json('Error in posting feedback')
+    }
+})
+
+module.exports = fb
